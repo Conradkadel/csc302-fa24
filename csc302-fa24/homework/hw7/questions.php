22717c0f847fbb1c773ef348ece958135edbefba
Conradkadel <123499188+Conradkadel@users.noreply.github.com>
2024-09-26 09:17:33 -0400

    Add files via upload

---
+++ csc302-fa24/homework/hw7/questions.php
@@ -0,0 +1,9 @@
+<?php 
+            $quiz = [
+                "What is this class calles?" => "CSC302",
+                "What is Teos first name?" => "toe",
+                "How many languages have we learned yet?" => "8",
+            
+            ];
+         
+?>
